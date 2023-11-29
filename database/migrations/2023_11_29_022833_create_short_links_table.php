85ace9305a16d714e050bbe1bf15655470409f9a
Oliver Servín <hey@oliverservin.com>
2023-11-28 20:42:09 -0600

    Create short link

---
+++ database/migrations/2023_11_29_022833_create_short_links_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('short_links', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('hashid')->unique();
+            $table->unsignedBigInteger('visits')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('short_links');
+    }
+};
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,51 @@
+<?php
+
+use App\Models\ShortLink;
+
+use Illuminate\Support\Str;
+use function Livewire\Volt\rules;
+use function Livewire\Volt\state;
+
+state([
+    'url' => '',
+]);
+
+rules(['url' => 'required|string|max:255']);
+
+$store = function () {
+    $this->validate();
+
+    ShortLink::create([
+        'url' => $this->url,
+        'hashid' => Str::random(5),
+    ]);
+
+    $this->url = '';
+}
+
+?>
+
 <x-app-layout>
     <div class="py-2 px-5">
-        Hello!
+        @volt
+            <form wire:submit="store">
+                <h2 class="font-bold">Crear enlace</h2>
+
+                <div class="space-y-2">
+                    <label for="url">URL:</label>
+
+                    <input
+                        wire:model="url"
+                        type="text"
+                        id="url"
+                        placeholder="https://"
+                        required
+                        autocomplete="off"
+                    >
+
+                    <button class="block border-2 p-1">Submit</button>
+                </div>
+            </form>
+        @endvolt
     </div>
 </x-app-layout>
