371e79fd909a89ed5aadfd71339002c4ac6b554e
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2023-12-27 22:57:20 -0600

    wip

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
 
+    <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32">
+    <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16">
+
     @vite(['resources/css/app.css', 'resources/app/app.js'])
 </head>
 <body class="bg-zinc-50 font-sans dark:bg-zinc-950">
--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -6,6 +6,9 @@
 
         <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32">
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
