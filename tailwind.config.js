d32658fc10f1e58aae5584a07f51ffeaed6fdf66
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2023-12-29 18:54:17 -0600

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
