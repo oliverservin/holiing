7d56e3ef68d4294773ba3e7bf5801bc86d768739
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2023-12-29 20:24:49 -0600

    wip

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -24,7 +24,9 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if (app()->bound('honeybadger')) {
+                app('honeybadger')->notify($e, app('request'));
+            }
         });
     }
 }
