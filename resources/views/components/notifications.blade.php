6a5df0172d4cb47c5deddf618260fb0ca844fbd9
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-01-03 21:31:20 -0600

    wip

--- resources/views/components/notifications.blade.php
+++ resources/views/components/notifications.blade.php
@@ -25,7 +25,7 @@
                 show: false,
                 init() {
                     this.$nextTick(() => this.show = true)
-                    setTimeout(() => this.transitionOut(), 2000)
+                    setTimeout(() => this.transitionOut(), 6000)
                 },
                 transitionOut() {
                     this.show = false
@@ -33,23 +33,19 @@
                 }"
             x-show="show"
             x-transition.duration.500ms
-            class="pointer-events-auto relative w-full max-w-sm rounded-md border border-gray-200 bg-white py-4 pl-6 pr-4 shadow-lg"
+            class="pointer-events-auto relative w-full max-w-sm rounded-md border border-zinc-200 bg-white py-4 pl-6 pr-4 shadow-lg"
         >
             <div class="flex items-start">
-                <div x-show="notification.type === 'success'" class="flex-shrink-0">
-                    <span aria-hidden="true" class="inline-flex h-6 w-6 items-center justify-center rounded-full border-2 border-green-600 text-lg font-bold text-green-600">&check;</span>
-                    <span class="sr-only">Success:</span>
-                </div>
-                <div x-show="notification.type === 'error'" class="flex-shrink-0">
-                    <span aria-hidden="true" class="inline-flex h-6 w-6 items-center justify-center rounded-full border-2 border-red-600 text-lg font-bold text-red-600">&times;</span>
+                <div x-show="notification.type === 'error'" class="inline-flex mt-1 mr-3 flex-shrink-0">
+                    <span aria-hidden="true" class="inline-flex h-4 w-4 items-center justify-center rounded-full border-2 border-red-600 text-xs font-bold text-red-600">&times;</span>
                     <span class="sr-only">Error:</span>
                 </div>
-                <div class="ml-3 w-0 flex-1 pt-0.5">
-                    <p x-text="notification.content" class="text-sm font-medium leading-5 text-gray-900"></p>
+                <div class="w-0 flex-1 pt-0.5">
+                    <p x-text="notification.content" class="text-sm font-medium leading-5 text-zinc-900"></p>
                 </div>
                 <div class="ml-4 flex flex-shrink-0">
-                    <button @click="transitionOut()" type="button" class="inline-flex text-gray-400">
-                        <svg aria-hidden class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <button @click="transitionOut()" type="button" class="inline-flex text-zinc-400">
+                        <svg aria-hidden class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                         </svg>
                         <span class="sr-only">Close notification</span>
