9a73e0817c0b274dc4e9e92cfcfd175495900330
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-01-04 19:27:45 -0600

    wip

--- resources/views/pages/settings/domains/create.blade.php
+++ resources/views/pages/settings/domains/create.blade.php
@@ -2,10 +2,29 @@
 
 use function Laravel\Folio\middleware;
 use function Laravel\Folio\name;
+use function Livewire\Volt\rules;
+use function Livewire\Volt\state;
 
 middleware('auth');
 
 name('settings.domains.create');
+
+state(['domain' => '']);
+
+rules(['domain' => ['required', 'string', 'max:255', 'regex:/^(?!-)([A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/']]);
+
+$store = function () {
+    $validated = $this->validate();
+
+    auth()->user()->currentTeam->domains()->create([
+        'name' => $validated['domain'],
+        'public_domain' => false,
+    ]);
+
+    session()->flash('flash.notification', 'Dominio agregado.');
+
+    $this->redirect(route('settings.domains.index'), navigate: true);
+};
 
 ?>
 
@@ -16,21 +35,36 @@ name('settings.domains.create');
     <x-slot:subheader>
         <x-settings-navbar />
     </x-slot:subheader>
-    <x-container>
-        <div class="flex justify-between items-end">
-            <div>
-                <h5 class="text-sm font-bold mb-2">
-                    <a href="{{ route('settings.domains.index') }}" wire:navigate class="underline">Dominios</a>
-                </h5>
-                <h1 class="text-[44px] font-extrabold tracking-tight leading-[1.15]">Agregar dominio</h1>
-            </div>
-        </div>
-        <div class="mt-6 pb-20">
-            <div class="p-8 bg-zinc-50">
-                <div class="max-w-lg">
-                    <livewire:domains.create />
+    @volt('pages.settings.domains.create')
+        <x-container>
+            <div class="flex justify-between items-end">
+                <div>
+                    <h5 class="text-sm font-bold mb-2">
+                        <a href="{{ route('settings.domains.index') }}" wire:navigate class="underline">Dominios</a>
+                    </h5>
+                    <h1 class="text-[44px] font-extrabold tracking-tight leading-[1.15]">Agregar dominio</h1>
                 </div>
             </div>
-        </div>
-    </x-container>
+            <div class="mt-6 pb-20">
+                <div class="p-8 bg-zinc-50">
+                    <div class="max-w-lg">
+                        <form wire:submit="store" class="w-full space-y-8">
+                            <x-fieldset>
+                                <x-fieldset.field-group>
+                                    <x-fieldset.field>
+                                        <x-fieldset.label>Dominio</x-fieldset.label>
+                                        <x-input wire:model="domain" id="domain" type="text" name="domain" placeholder="holi.ing" required />
+                                        @error('domain')
+                                            <x-fieldset.error-message>{{ $message }}</x-fieldset.error-message>
+                                        @enderror
+                                    </x-fieldset.field>
+                                </x-fieldset.field-group>
+                            </x-fieldset>
+                            <x-button>Agregar dominio</x-button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </x-container>
+    @endvolt
 </x-app-layout>
--- resources/views/pages/settings/domains/index.blade.php
+++ resources/views/pages/settings/domains/index.blade.php
@@ -1,11 +1,57 @@
 <?php
 
-use function Laravel\Folio\middleware;
 use function Laravel\Folio\name;
+use function Laravel\Folio\middleware;
+use function Livewire\Volt\state;
+
+use App\Livewire\InteractsWithNotifications;
+use App\Models\Domain;
+use App\Models\User;
+use App\Notifications\DomainVerificationRequested;
+use Illuminate\Support\Facades\Notification;
+use Spatie\Dns\Dns;
 
 middleware('auth');
 
 name('settings.domains.index');
+
+$getDomains = fn () => $this->domains = auth()->user()->currentTeam->domains()->latest()->get();
+
+uses(InteractsWithNotifications::class);
+
+state(['domains' => $getDomains, 'validating' => null]);
+
+$validateDomain = function (Domain $domain) {
+    $this->validating = $domain;
+
+    $dns = new Dns();
+
+    $dnsRecords = $dns->getRecords($domain->name, ['A', 'AAAA']);
+
+    $validRecord = collect($dnsRecords)->first(function ($record) {
+        if ($record->type() === 'A' && $record->ip() === '66.241.124.101') {
+            return true;
+        }
+
+        if ($record->type() === 'AAAA' && $record->ipv6() === '2a09:8280:1::69:d0e7') {
+            return true;
+        }
+    });
+
+    if($validRecord) {
+        $domain->validated_at = now();
+
+        Notification::send(User::admin()->get(), new DomainVerificationRequested($domain));
+
+        $domain->save();
+
+        $this->notify('El dominio fue validado correctamente.');
+    } else {
+        $this->notifyError('El dominio no fue validado correctamente.');
+    }
+
+    $this->getDomains();
+};
 
 ?>
 
@@ -16,17 +62,19 @@ name('settings.domains.index');
     <x-slot:subheader>
         <x-settings-navbar />
     </x-slot:subheader>
-    <x-container>
-        <div class="flex justify-between items-end">
-            <div>
-                <h1 class="text-5xl font-extrabold">Dominios</h1>
+    @volt('pages.settings.domains')
+        <x-container>
+            <div class="flex justify-between items-end">
+                <div>
+                    <h1 class="text-5xl font-extrabold">Dominios</h1>
+                </div>
+                <div>
+                    <x-button href="{{ route('settings.domains.create') }}">Agregar dominio</x-button>
+                </div>
             </div>
-            <div>
-                <x-button href="{{ route('settings.domains.create') }}">Agregar dominio</x-button>
+            <div class="mt-6 pb-20">
+                <livewire:domains.list />
             </div>
-        </div>
-        <div class="mt-6 pb-20">
-            <livewire:domains.list />
-        </div>
-    </x-container>
+        </x-container>
+    @endvolt
 </x-app-layout>
