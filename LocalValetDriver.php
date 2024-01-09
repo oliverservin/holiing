fad5f421eaa81554c9eae1c2b7aa7fec95d80ae4
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-01-08 18:05:33 -0600

    Create LocalValetDriver.php

---
+++ LocalValetDriver.php
@@ -0,0 +1,18 @@
+<?php
+
+use Valet\Drivers\LaravelValetDriver;
+
+class LocalValetDriver extends LaravelValetDriver
+{
+    public function beforeLoading(string $sitePath, string $siteName, string $uri): void
+    {
+        if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+            $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+        }
+
+        if ($siteName !== 'holiing.test') {
+            $_SERVER['HTTP_LINKSPAGEHOST'] = $_SERVER['HTTP_HOST'];
+            $_SERVER['HTTP_HOST'] = 'links.holiing.test';
+        }
+    }
+}
