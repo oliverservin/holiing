db6b39a1695fece7a72b2ddb21dd182d46fd08a0
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-01-26 17:24:48 -0600

    wip

--- resources/views/components/app/links/index/table.blade.php
+++ resources/views/components/app/links/index/table.blade.php
@@ -58,10 +58,6 @@
 </x-table>
 
 {{-- Pagination... --}}
-<div class="pt-4 flex justify-between items-center">
-    <div class="text-gray-700 text-sm">
-        Results: {{ \Illuminate\Support\Number::format($shortLinks->total()) }}
-    </div>
-
+<div class="mt-5 flex justify-end">
     {{ $shortLinks->links('livewire.links.index.pagination') }}
 </div>
