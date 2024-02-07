3d2a667ad59bff78ab01eab8d2dcd04070333381
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-02-06 19:25:30 -0600

    wip

--- resources/views/components/app/notifications.blade.php
+++ resources/views/components/app/notifications.blade.php
@@ -48,7 +48,7 @@
                         <div class="ml-4 flex flex-shrink-0">
                             <x-button @click="transitionOut()" type="button" plain>
                                 <span class="sr-only">Cerrar</span>
-                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <svg class="h-5 w-5" data-slot="icon" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
                                 </svg>
                             </x-button>
